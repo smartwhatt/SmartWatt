6da62eafa334c6d19e21cdfb3dd3337312a1d931
Jimmy_Tempest <smartwattana@gmail.com>
2021-09-02 19:06:54 +0700

    add footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,10 @@
+import styles from '../../styles/modules/footer.module.scss'
+
 
 export default function Footer() {
   return (
-    <>
-    </>
+    <footer className={styles["footer-container"]} >
+        <span className={styles["copyright"]}>&copy;Smart Wattanapornmongkol</span>
+    </footer>
   )
 }
