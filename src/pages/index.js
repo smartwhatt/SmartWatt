ebb145c5df72df92c3dbfe75079dc33fea1a6ce4
Jimmy_Tempest <smartwattana@gmail.com>
2021-09-06 18:06:41 +0700

    fix home image overright others

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className={styles.parallax}>
       <div className={styles["title"]}>
         <div className={styles["image"]}>
-        <Image src={"/img/profile.png"} width={240} height={240} objectFit='contain' alt={"This should've been where my picture is if it loads"} />
+          <Image className={styles["img"]} src={"/img/profile.png"} width={240} height={240} objectFit='contain' alt={"This should've been where my picture is if it loads"}  />
         </div>
         <h1 className={styles["name"]}>Smart Wattanapornmongkol</h1>
         <h2 className={styles["subtext"]}><ReactTypingEffect text={["Fullstack Developer", "Cloud Engineer", "DevOps Engineer"]} speed={150} eraseSpeed={150} typingDelay={1000} /></h2>
