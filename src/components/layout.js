0e1924c2c27816e6940111d9e39fa3b873016753
Jimmy_Tempest <smartwattana@gmail.com>
2021-09-06 18:51:58 +0700

    smoother page transition

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,36 +2,36 @@
 import dynamic from 'next/dynamic'
 const Navbar = dynamic(() => import('./navbar'))
 const Footer = dynamic(() => import('./footer'))
+import { motion } from "framer-motion"
+import { AnimatePresence } from 'framer-motion'
 
 // React import
 import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+
 
 export default function Layout({ children }) {
   const router = useRouter();
-  const [displayChildren, setDisplayChildren] = useState(children);
-  const [transitionStage, setTransitionStage] = useState("fadeOut");
 
-  useEffect(() => {
-    setTransitionStage("fadeIn");
-  }, []);
-
-  useEffect(() => {
-    if (children !== displayChildren) setTransitionStage("fadeOut");
-  }, [children, setDisplayChildren, displayChildren]);
-
-  function fadeHandle(){
-    if (transitionStage === "fadeOut") {
-      // console.log("fading out");
-      setDisplayChildren(children);
-      setTransitionStage("fadeIn");
-    }
-  }
+  const variants = {
+    hidden: { opacity: 0,},
+    enter: { opacity: 1},
+    exit: { opacity: 0},
+}
 
   return (
     <>
       <Navbar page={router.asPath} />
-      <main className={transitionStage} onTransitionEnd={() => fadeHandle()}>{displayChildren}</main>
+        <motion.main
+          variants={variants} // Pass the variant object into Framer Motion 
+          initial="hidden" // Set the initial state to variants.hidden
+          animate="enter" // Animated state to variants.enter
+          exit="exit" // Exit state (used later) to variants.exit
+          transition={{ duration: 1, ease: "easeInOut" }} // Set the transition to linear
+          key={router.route}
+        >
+          {children}
+        </motion.main>
       <Footer />
     </>
   )
