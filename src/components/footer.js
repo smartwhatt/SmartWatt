0d8a30df1cb249ba19dfc496ac90be3965effc32
Jimmy_Tempest <smartwattana@gmail.com>
2021-09-29 15:39:31 +0700

    make skills badges standard

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,9 +12,9 @@ export default function Footer() {
       <hr className={styles["divider"]} />
         <span className={styles["copyright"]}>&copy; Smart Wattanapornmongkol {new Date().getFullYear()}</span>
         <div className={styles["social"]}>
-            <a href="https://www.linkedin.com/in/smart-wattanapornmongkol-b3b758207/"><AiFillLinkedin className={styles["icon"]} size={24} /></a>
-            <a href="https://discord.com/users/508507551999197225"><RiDiscordFill className={styles["icon"]} size={24} /></a>
-            <a href="https://github.com/Jimmy-Tempest"><AiFillGithub className={styles["icon"]} size={24} /></a>
+            <a title="Linkedin" href="https://www.linkedin.com/in/smart-wattanapornmongkol-b3b758207/"><AiFillLinkedin className={styles["icon"]} size={24} /></a>
+            <a title="Discord" href="https://discord.com/users/508507551999197225"><RiDiscordFill className={styles["icon"]} size={24} /></a>
+            <a title="Github" href="https://github.com/Jimmy-Tempest"><AiFillGithub className={styles["icon"]} size={24} /></a>
         </div>
     </footer>
     </>
