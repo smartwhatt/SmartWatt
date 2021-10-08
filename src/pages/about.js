5c6329b9fb9481e3ddb25f66c08828e25776b4cd
Jimmy_Tempest <smartwattana@gmail.com>
2021-10-09 00:29:48 +0700

    update to better version of react reveal

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,8 @@ import styles from '../../styles/modules/about.module.scss'
 import dynamic from 'next/dynamic'
 import {useRouter} from 'next/router'
 import firebase from "../libs/clientApp"
-import {useCollection} from "react-firebase-hooks/firestore"
 
-import Fade from 'react-reveal/Fade';
+import { Fade } from "react-awesome-reveal";
 
 
 const Title = dynamic(() => import('../components/title'))
@@ -33,21 +32,26 @@ export default function About({data}) {
         <title>SmartWatt | About</title>
       </Head>
       <Title title="About Smart" path={router.asPath} />
-      <Fade bottom cascade>
         <div>
-            <h3 className={styles["h3"]}>About Me</h3>
-            <p className={styles["paragraph"]}>I am just a high school student at Suankularb Wittayalai School, Thailand, who is interested in software development and computer science. Currently, I am working mostly with Python in field of Data Science and some web development with Django and Javascript framework like Nextjs and Reactjs.</p>
+        <Fade direction="up" cascade triggerOnce={true} damping={0.1} >
+              <h3 className={styles["h3"]}>About Me</h3>
+              <p className={styles["paragraph"]}>My name is Smart Wattanapornmongkol and my nickname is Best (yes my name is both Best and Smart) but people often call me Jimmy (Very random, I know). I am currently studying in Eplus+ program at Suankularb Wittayalai School, Thailand.</p>
+              <p className={styles["paragraph"]}>I considered myself a fullstack developer, but sometimes I also work with AI and machine learning. Though, I am willing to try doing other things as well.</p>
+              <p className={styles["paragraph"]}>I got into programming while I was in grade 7. I was introduced to python programming by my classmate’s father who is software engineer.  I have been into programming for {new Date().getFullYear() - 2017} years. Now, I am working with various programming language and frameworks, mainly Javascript’s Next.js and Python’s Django.</p>
+        </Fade>
         </div>
-      </Fade>
+      
       <div>
-        <Fade bottom>
+        <Fade direction="up" triggerOnce={true}>
           <h3 className={styles["h3"]}>Expertise</h3>
           <p className={styles["expertise-paragraph"]}>I have experience with many tools and programming language which I listed some that I comfortable with below</p>
         </Fade>
           <div className={styles["skills"]}>
+          <Fade  triggerOnce={true} cascade damping={0.05}>
             {data.map((skill, index) => {
-              return <Fade key={index}><Skill name={skill.name} src={skill.src} /></Fade>
+              return <Skill key={index} name={skill.name} src={skill.src}  />
             })}
+          </Fade>
           </div>
       </div>
       
